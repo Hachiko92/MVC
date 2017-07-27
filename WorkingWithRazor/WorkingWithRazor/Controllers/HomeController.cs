b67bffc4354bc1acd24c2e1205225d810ae8a8dc
Hachiko92 <eleonora.franchini@tajamar365.com>
2017-07-27 18:52:42 +0200

    ejercicio

--- WorkingWithRazor/WorkingWithRazor/Controllers/HomeController.cs
+++ WorkingWithRazor/WorkingWithRazor/Controllers/HomeController.cs
@@ -10,7 +10,26 @@ namespace WorkingWithRazor.Controllers
     {
         public ActionResult Index()
         {
+            string[] frutas = new string[] { "Apple", "Orange", "Pear" };
+            return View(frutas);
+        }
+
+        public ActionResult List()
+        {
             return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        /* el metodo que aparece debaho de [ChildActionOnly] es lo que lo utiliza
+         * el ChildAction siempre se asocia a una PartialView */
+        [ChildActionOnly]
+        public ActionResult Time()
+        {
+            return PartialView(DateTime.Now);
         }
     }
 }
